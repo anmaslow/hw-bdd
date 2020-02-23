f634f1721ed168649ab98c05fd4c6b4fbff52507
Anastasia <ann.maslow@gmail.com>
2020-02-23 16:54:49 +0300

    add login test

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,23 @@
+package ru.netology.web.test;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+import static com.codeborne.selenide.Selenide.open;
+
+class MoneyTransferTest {
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV2() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPage();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+
+}
+
